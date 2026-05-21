0f74313058a16e2fd33fdf61bd1dcd3aea760ab0
tanjidahmad <tanjidahmad.cse.uiu@gmail.com>
2026-05-21 12:41:10 -0400

    sove

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,4 @@
 
-// /** @type {import('next').NextConfig} */
-
-// const nextConfig = {
-
-//   reactCompiler: true,
-
-//   images: {
-
-//     domains: [
-//       "images.unsplash.com",
-//     ],
-
-//   },
-
-// };
-
-// export default nextConfig;
-
 
 /** @type {import('next').NextConfig} */
 
