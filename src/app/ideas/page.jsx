ef51cdb40600ba0c7c72da8acd45bc1709b1651f
tanjidahmad <tanjidahmad.cse.uiu@gmail.com>
2026-05-21 12:20:41 -0400

    ideas

--- src/app/ideas/page.jsx
+++ src/app/ideas/page.jsx
@@ -1,6 +1,5 @@
 import IdeasContainer from "@/components/IdeasContainer";
-// import { auth } from "@/lib/auth";
-// import { headers } from "next/headers";
+
 
 
 export const metadata = {
@@ -8,10 +7,7 @@ export const metadata = {
 };
 export default async function IdeasPage() {
 
-// const {token}=await auth.api.getToken({
 
-//     headers:await headers()
-//   });
 
   const res = await fetch(
 
@@ -19,9 +15,7 @@ export default async function IdeasPage() {
 
     `${process.env.NEXT_PUBLIC_SERVER_URL}/ideas`,
 
-    // {
-    //       headers:{ authorization:`Bearer ${token}`}
-    //     },
+   
 
     {
       cache: "no-store",
